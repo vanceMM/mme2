28d6b86a84ff05125d3ce68d27a0e982981d0b82
Ben Awesome <baetschjunge@googlemail.com>
2016-04-13 02:15:27 +0200

    update html,css,js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 document.addEventListener("DOMContentLoaded", domLoaded(), false);
 
 function domLoaded(e) {
-
-    playVideoOnClick();
     showMenu();
+    playVideoOnClick();
+    stopVideoOnCLick();
+    showHideVideos();
 };
 
 function playVideoOnClick(){
-  var btnListe = document.getElementsByClassName("playBtn");
+    var btnListe = document.getElementsByClassName("playBtn");
     var videoListe = document.getElementsByClassName("video");
 
     var videoCount = videoListe.length;
@@ -20,7 +21,7 @@ function playVideoOnClick(){
       vidEl[i].id = ["video"+i];
       //console.log(el[i]);
 
-      btnListe[i].id = ["btn"+i];
+      btnListe[i].id = ["playBtn"+i];
       //console.log(buttons[i]);
 
       btnListe[i].addEventListener("click", function(){
@@ -42,8 +43,59 @@ function playVideoOnClick(){
           video2play.pause();
           btnEl.innerHTML = "Play";
         }
-      }); // end of eventhandler      
+      }); // end of eventhandler
+
+      // yolo video click listener
+      videoListe[i].addEventListener("click", function(){
+       
+        var clickedVid = this.id;
+        //console.log(clickedBtn);        
+        var vidIndex = clickedVid.substr(clickedVid.length - 1);
+        //console.log(btnIndex);
+        var video2play = document.getElementById("video"+vidIndex);
+        //console.log(video2play);
+        var vidEl = document.getElementById(clickedVid);
+
+        if (video2play.paused){
+          video2play.play();
+        }
+        else {
+          video2play.pause();
+        }
+      }); // end of eventhandler       
     } // end of for
+}
+
+function stopVideoOnCLick(){
+  var btnListe = document.getElementsByClassName("stopBtn");
+  var videoListe = document.getElementsByClassName("video");
+
+  var videoCount = videoListe.length;
+  // loop through button array
+
+  for (var i = 0; i < videoCount; i++){
+    var vidEl = document.getElementsByClassName("video");
+
+    vidEl[i].id = ["video"+i];
+    //console.log(el[i]);
+
+    btnListe[i].id = ["stopBtn"+i];
+    //console.log(buttons[i]);
+
+    btnListe[i].addEventListener("click", function(){
+     
+      var clickedBtn = this.id;
+      //console.log(clickedBtn);        
+      var btnIndex = clickedBtn.substr(clickedBtn.length - 1);
+      //console.log(btnIndex);
+      var video2play = document.getElementById("video"+btnIndex);
+      //console.log(video2play);
+      var btnEl = document.getElementById(clickedBtn);
+
+      video2play.pause();
+      video2play.currentTime = 0;
+    }); // end of eventhandler      
+  } // end of for
 }
 
 function showMenu(){
@@ -54,4 +106,11 @@ function showMenu(){
     bodyEl.classList.toggle('active-nav');
     //e.preventDefault();
   });
+}
+
+function showHideVideos(){
+    var nav = document.querySelector("nav");
+    console.log(nav);
+    var navItems = nav.querySelectorAll("li");
+    console.log(navItems);
 }
