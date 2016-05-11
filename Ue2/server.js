3b3a9c3f7e66177a9b70b3bd93e1ed8d6ebd1586
Valentin Risch <valentin_risch@hotmail.de>
2016-05-11 14:09:40 +0200

    Ue3 start code

--- Ue2/server.js
+++ Ue2/server.js
@@ -1,3 +1,4 @@
+// @author: doge
 /* Loading the required modules*/
 
 var express     = require('express');
@@ -6,38 +7,43 @@ var path        = require('path');
 var fs          = require('fs');
 var hrstart     = process.hrtime();
 
-/*  use express middleware express.static
-    with /static path prefix
-*/
-app.use('/static', express.static(__dirname + '/static'));
-
-
-/*  use express middleware express.static
-    without static path prefix
-*/
-//app.use(express.static('../Ue2'));
-
-/*routing any filename input to a filereader, if the file does not exists the readFile methodes throws
-an error and goes on with the next route*/
 /*
+    use express middleware express.static
+    with /static path prefix and absolute path
+    add route to static files
+*/
+//app.use('/static', express.static(__dirname + '/static'));
+
+/*
+    use express middleware express.static
+    without path prefix,
+    with absolute path
+    add route to static files
+*/
+//app.use(express.static(path.join(__dirname, '../Ue2 ')));
+
+
+/*
+    routing any filename input to a filereader, if the file does not exists the readFile methodes throws
+    an error and goes on with the next route.
+    /:name is the property available at req.params.name.
+*/
+
 app.get('/:name', function (req, res, next) {
   var fileName = req.params.name;
-
-  fs.readFile(fileName,'utf8', function(err, data) {
-  if (err) {
-  console.log('file was not found');
-  return next();
-}
-  //current high-resolution real time
   var hrend = process.hrtime(hrstart);
+  fs.readFile(fileName,'utf8', function(err, data) {
+    if (err) {
+      console.log("file was not found");
+      return next();
+    }
+    //current high-resolution real time
 
-  res.send(''+data+ '\n' + 'Execution time (hr): ' + hrend[1]/1000000 + 'ms' );
+    res.send(''+data+ '\n' + 'Execution time (hr): ' + hrend[1]/1000000 + 'ms' );
 
   });
 
 });
-*/
-
 
 app.get('/time', function (req, res) {
   //sets the Content-Type of the respond object
@@ -45,6 +51,15 @@ app.get('/time', function (req, res) {
   res.send(new Date().toLocaleTimeString());
 });
 
+/*
+    use express middleware express.static
+    without static path prefix and relative path
+*/
+app.use(express.static('../Ue2'));
+
+/*
+    match every url and respond "hello world"
+*/
 app.get('*', function(req, res) {
   res.send('Hello World!');
 });
