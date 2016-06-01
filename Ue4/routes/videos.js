91799c099edef7e74dc86078a27e2f13696c5d1f
Valentin Risch <valentin_risch@hotmail.de>
2016-06-01 14:23:30 +0200

    videos_rest_api_1a_test delte tests still failing

---
+++ Ue4/routes/videos.js
@@ -0,0 +1,76 @@
+/** This module defines the routes for videos using the store.js as db memory
+ *
+ * @author Johannes Konert
+ * @licence CC BY-SA 4.0
+ *
+ * @module routes/videos
+ * @type {Router}
+ */
+
+// remember: in modules you have 3 variables given by CommonJS
+// 1.) require() function
+// 2.) module.exports
+// 3.) exports (which is module.exports)
+
+// modules
+var express = require('express');
+var logger = require('debug')('me2u4:videos');
+var store = require('../blackbox/store');
+
+var videos = express.Router();
+
+// if you like, you can use this for task 1.b:
+var requiredKeys = {title: 'string', src: 'string', length: 'number'};
+var optionalKeys = {description: 'string', playcount: 'number', ranking: 'number'};
+var internalKeys = {id: 'number', timestamp: 'number'};
+
+
+// routes **********************
+videos.route('/')
+    .get(function(req, res, next) {
+        // TODO
+        next();
+    })
+    .post(function(req,res,next) {
+        var video = store.insert('videos', req.body);
+        res.status(201).json(store.select('videos', video));
+    });
+videos.route('/:id')
+    .put(function (req, res, next) {
+       store.replace('videos', req.params.id, req.body);
+       res.json(store.select('videos', req.params.id));
+    })
+    .delete(function (req, res, next) {
+        var video = store.select('videos', req.params.id);
+        if(video == undefined) {
+            res.status(400).end();
+        }
+        else {
+            store.remove('videos', req.params.id);
+            res.status(204).end();
+        }
+});
+// route that catches all the others(wrong ones)
+videos.route('/*')
+    .all(function (req, res, next) {
+        res.status(405).end();
+
+    });
+
+
+
+// this middleware function can be used, if you like (or remove it)
+/*videos.use(function(req, res, next){
+    // if anything to send has been added to res.locals.items
+    if (res.locals.items) {
+        // then we send it as json and remove it
+        res.json(res.locals.items);
+        delete res.locals.items;
+    } else {
+        // otherwise we set status to no-content
+        res.set('Content-Type', 'application/json');
+        res.status(204).end(); // no content;
+    }
+});*/
+
+module.exports = videos;
