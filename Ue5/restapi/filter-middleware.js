09a03690f345fe0ea8bbee9fd6791b8c463e80e7
ban jamien <baetschjunge@googlemail.com>
2016-06-21 20:00:46 +0200

    Ue5: Aufg.1 done  / aufg.2 half done / start bonus

---
+++ Ue5/restapi/filter-middleware.js
@@ -0,0 +1,15 @@
+/**
+ * Created by baetschjunge on 21/06/16.
+ */
+
+
+
+var router = require('express').Router();
+var logger = require('debug')('me2u4:filterware');
+
+
+router.use(function (req, res, next) {
+
+});
+
+module.exports = router;
